20fb119c889e9fc5cbf6dffc59c4007668de64c0
Kyle Mayor <kylemayor@gmail.com>
2022-05-22 08:36:20 -0500

    Added documentation and cleaned up code

--- controllers/setController.js
+++ controllers/setController.js
@@ -1,6 +1,12 @@
 const scry = require('scryfall-sdk');
 const fuzzysort = require('fuzzysort');
 
+/**
+ * Retrieves sets that match a given search parameter.
+ * @param {object} req Express request object 
+ * @param {object} res Express response object 
+ * @returns {json} JSON string of an array of scryFall set objects.
+ */
 exports.find = async (req, res) => {
   const query = req.params.query;
 
@@ -23,15 +29,27 @@ exports.find = async (req, res) => {
   return  res.send(set);
 }
 
+/**
+ * Retrieves a set by a given uuid.
+ * @param {object} req Express request object 
+ * @param {object} res Express response object
+ * @returns {json} JSON string of an array containing a ScryFall Set.
+ */
 exports.get = async (req, res) => {
   const set = await scry.Sets.byId(req.params.id);
   set.cards = await set.getCards()
 
-  res.send(set);
+  return res.send(set);
 }
 
+/**
+ * Returns all sets 
+ * @param {object} req Express request object
+ * @param {object} res Express response object
+ * @returns {json} JSON string of an array of scryFall set objects.
+ */
 exports.index = async (req, res) => {
   const sets = await scry.Sets.all();
 
-  res.send(sets);
+  return res.send(sets);
 }
