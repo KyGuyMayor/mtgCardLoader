750039ae258e48c74fdbf4001595e521067ab81f
Kyle Mayor <kylemayor@gmail.com>
2023-05-10 21:07:07 -0500

    Added tests for card controller

--- controllers/__tests__/cardControll.test.js
+++ controllers/__tests__/cardControll.test.js
@@ -17,5 +17,18 @@ describe('CardController', () => {
       expect(scry.Cards.byId).toHaveBeenCalledWith(req.params.id);
       expect(res.send).toHaveBeenCalledTimes(1);
     });
+
+    it('reutrns 404 not found when given an invalid scryfall card id', async () => {
+      scry.Cards.byId = jest.fn().mockRejectedValue(new Error('not found'));
+
+      const req = { params: { id: '1' } };
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn(), send: jest.fn() };
+
+      await cardController.get(req, res);
+
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.send).toHaveBeenCalledWith('Not Found');
+    })
   });
 });
