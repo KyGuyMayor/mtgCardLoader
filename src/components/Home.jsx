2546b4a6de6af0309334300eb2901670e5ba758f
Kyle Mayor <kylemayor@gmail.com>
2026-02-01 20:38:54 -0600

    Search and file fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CustomProvider } from 'rsuite';
 
 import NavigationBar from './Shared/NavigationBar';
+import ViewCard from './Shared/Card/ViewCard';
 
 /**
  * Renders the home page of the website
@@ -9,11 +10,23 @@ import NavigationBar from './Shared/NavigationBar';
  */
 const Home = () => {
   const [active, setActive] = useState('home');
+  const [randomCard, setRandomCard] = useState(null);
+
+  useEffect(() => {
+    const fetchRandomCard = async () => {
+      const results = await fetch('/cards/random');
+      const data = await results.json();
+      setRandomCard(data);
+    };
+
+    fetchRandomCard();
+  }, []);
 
   return (
     <CustomProvider theme="dark">
       <NavigationBar active={active} setActive={setActive} />
       <div>Welcome to MTG Card Loader</div>
+      {randomCard && <ViewCard card={randomCard} />}
     </CustomProvider>
   );
 };
