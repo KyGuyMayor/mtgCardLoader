d73ad2d217ff4eb0ec3ec3ee13f4689c68fd7cc0
Kyle Mayor <kylemayor@gmail.com>
2026-02-15 12:51:03 -0600

    Fixed Runtime Errors (#36)

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,32 @@
-const { override, addLessLoader } = require('customize-cra');
+const { override, addLessLoader, overrideDevServer } = require('customize-cra');
 
-module.exports = override(
-  addLessLoader({
-    // If you are using less-loader@5 or older version, please spread the lessOptions to options directly.
-    lessOptions: {
-      javascriptEnabled: true,
-      modifyVars: { '@base-color': '#f44336' }
-    }
-  })
-);
+const devServerConfig = () => (config) => {
+  config.client = {
+    ...config.client,
+    overlay: {
+      ...(config.client?.overlay || {}),
+      runtimeErrors: (error) => {
+        if (
+          error?.message ===
+          'ResizeObserver loop completed with undelivered notifications.'
+        ) {
+          return false;
+        }
+        return true;
+      },
+    },
+  };
+  return config;
+};
+
+module.exports = {
+  webpack: override(
+    addLessLoader({
+      lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: { '@base-color': '#f44336' },
+      },
+    })
+  ),
+  devServer: overrideDevServer(devServerConfig()),
+};
